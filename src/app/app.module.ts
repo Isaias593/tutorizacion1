4b80246479463b916337b2bb994eb37eeb94ec97
Isaias Coronel Zapa <58237470+Isaias593@users.noreply.github.com>
2023-05-17 09:49:51 -0500

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatTableModule } from '@angular/material/table';
 import {FormsModule} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent  
+    FooterComponent  ,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +25,8 @@ import { FooterComponent } from './footer/footer.component';
     MatInputModule,
     MatButtonModule,
     FormsModule,
+    ReactiveFormsModule,
+   
     
 
   ],
